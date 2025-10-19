2f744eb2c1812c473159f30fe1b5ee976d328fea
yuuich.nishida <yasukikudan@gmail.com>
2025-10-19 22:27:45 +0900

    Fix session builder signature

--- sqlserver/server.go
+++ sqlserver/server.go
@@ -1,22 +1,31 @@
 package sqlserver
 
 import (
+	"context"
 	"log"
 
 	sqle "github.com/dolthub/go-mysql-server"
-	"github.com/dolthub/go-mysql-server/memory"
 	"github.com/dolthub/go-mysql-server/server"
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/vitess/go/mysql"
 )
 
-// Start launches a MySQL-compatible server backed by the provided DbProvider on the given address.
-func Start(pro *memory.DbProvider, addr string) error {
+// Start launches a MySQL-compatible server backed by the provided DatabaseProvider on the given address.
+func Start(pro sql.DatabaseProvider, addr string) error {
 	engine := sqle.NewDefault(pro)
 	cfg := server.Config{
 		Protocol: "tcp",
 		Address:  addr,
 	}
-	s, err := server.NewServer(cfg, engine, sql.NewContext, memory.NewSessionBuilder(pro), nil)
+	sessionBuilder := server.SessionBuilder(func(ctx context.Context, conn *mysql.Conn, addr string) (sql.Session, error) {
+		base, err := sql.BaseSessionFromConnection(ctx, conn, addr)
+		if err != nil {
+			return nil, err
+		}
+		return base, nil
+	})
+
+	s, err := server.NewServer(cfg, engine, sql.NewContext, sessionBuilder, nil)
 	if err != nil {
 		return err
 	}
